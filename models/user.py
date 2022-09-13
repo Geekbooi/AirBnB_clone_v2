fdd4e96e7291516d6fec7316273b7a16e9e125f1
Geekbooi <yosephmekonnen@gmail.com>
2022-09-13 14:55:30 +0300

    python

--- models/user.py
+++ models/user.py
@@ -1,18 +1,32 @@
 #!/usr/bin/python3
-from models import *
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship, backref
+"""
+    module containing user class
+    module containing user class
+"""
+from models.base_model import BaseModel, Base
+import models
+from os import getenv
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine, Column, Integer, String
 
+storage_t = getenv("HBNB_TYPE_STORAGE")
 
 class User(BaseModel, Base):
-    __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
+    """
+        User class for the user
+        User class for the user
+    """
+    if (storage_t == "db"):
+        __tablename__ = "users"
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=True)
+        last_name = Column(String(128), nullable=True)
 
-    places = relationship("Place", backref="user",
-                          cascade="all, delete, delete-orphan")
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        places = relationship("Place", back_populates="user")
+        reviews = relationship("Review", back_populates="user")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
