c4c43149c3656d61768a800bd9d96a18679f9ac8
Geekbooi <yosephmekonnen@gmail.com>
2022-09-14 09:58:36 +0300

    updated

--- models/user.py
+++ models/user.py
@@ -4,26 +4,27 @@
     module containing user class
 """
 from models.base_model import BaseModel, Base
+import models
+from os import getenv
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine, Column, Integer, String
-from os import environ
 
-storage_engine = environ.get("HBNB_TYPE_STORAGE")
-
+storage_t = getenv("HBNB_TYPE_STORAGE")
 
 class User(BaseModel, Base):
     """
         User class for the user
         User class for the user
     """
-    if (storage_engine == 'db'):
+    if (storage_t == "db"):
         __tablename__ = "users"
         email = Column(String(128), nullable=False)
         password = Column(String(128), nullable=False)
         first_name = Column(String(128), nullable=True)
         last_name = Column(String(128), nullable=True)
-        places = relationship("Place", backref="user")
-        reviews = relationship("Review", backref="user")
+
+        places = relationship("Place", back_populates="user")
+        reviews = relationship("Review", back_populates="user")
     else:
         email = ""
         password = ""
