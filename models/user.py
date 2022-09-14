b523b4e27904cb4e3cfed6b66d757ec76be0217c
Geekbooi <yosephmekonnen@gmail.com>
2022-09-14 10:01:59 +0300

    updated

--- models/user.py
+++ models/user.py
@@ -1,18 +1,29 @@
 #!/usr/bin/python3
-from models import *
-from sqlalchemy import Column, String
+"""This is the user class"""
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
 from sqlalchemy.orm import relationship, backref
 
 
 class User(BaseModel, Base):
-    __tablename__ = "users"
+    """This is the class for user
+    Attributes:
+        email: email address
+        password: password for you login
+        first_name: first name
+        last_name: last name
+    """
+
+    __tablename__ = 'users'
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
 
-    places = relationship("Place", backref="user",
+    places = relationship("Place",
+                          backref="user",
                           cascade="all, delete, delete-orphan")
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    reviews = relationship("Review",
+                           backref="user",
+                           cascade="all, delete, delete-orphan")
