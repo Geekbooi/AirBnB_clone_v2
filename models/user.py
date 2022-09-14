e6f2af4640ddaed23080ca31b85f3a7351928065
Geekbooi <yosephmekonnen@gmail.com>
2022-09-14 10:00:12 +0300

    updated

--- models/user.py
+++ models/user.py
@@ -1,32 +1,18 @@
 #!/usr/bin/python3
-"""
-    module containing user class
-    module containing user class
-"""
-from models.base_model import BaseModel, Base
-import models
-from os import getenv
-from sqlalchemy.orm import relationship
-from sqlalchemy import create_engine, Column, Integer, String
+from models import *
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
 
-storage_t = getenv("HBNB_TYPE_STORAGE")
 
 class User(BaseModel, Base):
-    """
-        User class for the user
-        User class for the user
-    """
-    if (storage_t == "db"):
-        __tablename__ = "users"
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
 
-        places = relationship("Place", back_populates="user")
-        reviews = relationship("Review", back_populates="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    places = relationship("Place", backref="user",
+                          cascade="all, delete, delete-orphan")
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
