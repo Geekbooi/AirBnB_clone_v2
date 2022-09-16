96cd173f37fd0dec5b872b5bcefe981f09b5cf8f
Geekbooi <yosephmekonnen@gmail.com>
2022-09-17 00:21:27 +0300

    airbnb

--- models/user.py
+++ models/user.py
@@ -1,29 +1,16 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, MetaData
-from sqlalchemy.orm import relationship, backref
+"""This module defines a class User"""
+from sqlalchemy import String, Column
+from models.base_model import Base, BaseModel
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-        email: email address
-        password: password for you login
-        first_name: first name
-        last_name: last name
-    """
-
-    __tablename__ = 'users'
+    """This class defines a user by various attributes"""
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128))
-    last_name = Column(String(128))
-
-    places = relationship("Place",
-                          backref="user",
-                          cascade="all, delete, delete-orphan")
-
-    reviews = relationship("Review",
-                           backref="user",
-                           cascade="all, delete, delete-orphan")
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship("Place", backref="user")
+    reviews = relationship("Review", backref="user")
